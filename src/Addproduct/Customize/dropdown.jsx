6883c7895a8b5b3732a25ab811b8f92a50b094be
MEETSIDHDHAPURA <meetgajjar9976@gmail.com>
2024-04-01 15:07:21 +0530

    change

--- src/Addproduct/Customize/dropdown.jsx
+++ src/Addproduct/Customize/dropdown.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import { Box, Typography, TextField, Button, IconButton } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,27 +8,38 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import AddIcon from "@mui/icons-material/Add";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-import DehazeOutlinedIcon from "@mui/icons-material/DehazeOutlined";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import DehazeIcon from "@mui/icons-material/Dehaze";
 const Customdropdown = () => {
   const [rows, setRows] = useState([]);
-
+  const [defaultImage, setDefaultImage] = useState("./image/Icon.png");
   const handleAddVariant = () => {
     const newRow = {
       id: rows.length + 1,
       label: "",
       chams: "",
       price: "",
+      image: null,
     };
     setRows([...rows, newRow]);
   };
-
   const handleDeleteRow = (id) => {
     const updatedRows = rows.filter((row) => row.id !== id);
     setRows(updatedRows);
   };
-
+  const handleImageUpload = (event, id) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const updatedRows = rows.map((row) =>
+        row.id === id ? { ...row, image: reader.result } : row
+      );
+      setRows(updatedRows);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <Box>
       <Box sx={{ display: "flex", gap: "30px" }}>
@@ -43,6 +54,37 @@ const Customdropdown = () => {
             required
           />
         </Box>
+        <Box sx={{ mt: "28px" }}>
+          <Button
+            sx={{
+              height: "38px",
+              width: "200px",
+              marginLeft: "2%",
+              fontSize: "13px",
+              ml: "320px",
+            }}
+            variant="outlined"
+            startIcon={<AddIcon />}
+          >
+            Choose as Templet
+          </Button>
+        </Box>
+        <Box sx={{ mt: "28px" }}>
+          <Button
+            sx={{
+              height: "38px",
+              width: "200px",
+              marginLeft: "2%",
+              fontSize: "13px",
+            }}
+            variant="outlined"
+            startIcon={<AddIcon />}
+          >
+            Save as Templet
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", gap: "30px", mt: "10px" }}>
         <Box>
           <Typography>Instruction</Typography>
           <TextField
@@ -50,12 +92,18 @@ const Customdropdown = () => {
             placeholder="Write instruction here..."
             size="small"
             variant="outlined"
-            sx={{ width: "450px", mt: "5px" }}
+            sx={{ width: "900px", mt: "5px" }}
             required
           />
         </Box>
         <Box sx={{ mt: "28px" }}>
           <Button
+            sx={{
+              height: "38px",
+              width: "150px",
+              marginLeft: "2%",
+              fontSize: "13px",
+            }}
             variant="outlined"
             startIcon={<AddIcon />}
             onClick={handleAddVariant}
@@ -64,13 +112,19 @@ const Customdropdown = () => {
           </Button>
         </Box>
       </Box>
-
       <Box sx={{ mt: "30px" }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableContainer sx={{ tableLayout: "fixed" }}>
+          <Table
+            sx={{ minWidth: 650, tableLayout: "fixed" }}
+            size="small"
+            aria-label="a dense table"
+          >
             <TableHead sx={{ bgcolor: "lightgray" }}>
               <TableRow>
-                <TableCell align="center"></TableCell>
+                <TableCell
+                  align="left"
+                  sx={{ "&.css-fp0xi9-MuiTableCell-root": { width: "30%" } }}
+                ></TableCell>
                 <TableCell align="center">Lable</TableCell>
                 <TableCell align="center">Chams</TableCell>
                 <TableCell align="center">Price</TableCell>
@@ -78,20 +132,58 @@ const Customdropdown = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell align="center">
-                    <DehazeOutlinedIcon />
+              {rows.map((row, index) => (
+                <TableRow key={index} sx={{ alignItems: "center" }}>
+                  <TableCell
+                    sx={{ "&.css-8fdh2x-MuiTableCell-root": { width: "30%" } }}
+                    align="left"
+                  >
+                    <Box>
+                      <DehazeIcon />
+                    </Box>
+                  </TableCell>
+                  <TableCell align="left" sx={{ display: "flex", gap: "10px" }}>
+                    <Box>
+                      <TextField
+                        sx={{ py: "6px" }}
+                        size="small"
+                        placeholder="Enter Chams name.."
+                      ></TextField>
+                    </Box>
                   </TableCell>
                   <TableCell align="center">
-                    <TextField placeholder="Enter Chams name.."></TextField>
+                    <label htmlFor={`upload-image-${row.id}`}>
+                      <input
+                        type="file"
+                        id={`upload-image-${row.id}`}
+                        accept="image/*"
+                        onChange={(event) => handleImageUpload(event, row.id)}
+                        style={{ display: "none" }}
+                      />
+                      <img
+                        src={row.image ? row.image : defaultImage}
+                        alt="image"
+                        style={{
+                          objectFit: "contain",
+                          height: "40%",
+                          width: "40%",
+                          cursor: "pointer",
+                          aspectRatio: "16/9",
+                        }}
+                      />
+                    </label>
                   </TableCell>
-                  <TableCell align="center">{row.chams}</TableCell>
-                  <TableCell align="center">{row.price}</TableCell>
                   <TableCell align="center">
-                    <Button onClick={() => handleDeleteRow(row.id)}>
-                      <DeleteOutlineOutlinedIcon />
-                    </Button>
+                    <TextField
+                      size="small"
+                      placeholder="00"
+                      sx={{ width: "70px" }}
+                    />
+                  </TableCell>
+                  <TableCell align="center">
+                    <IconButton onClick={() => handleDeleteRow(row.id)}>
+                      <DeleteIcon />
+                    </IconButton>
                   </TableCell>
                 </TableRow>
               ))}
@@ -102,5 +194,4 @@ const Customdropdown = () => {
     </Box>
   );
 };
-
 export default Customdropdown;
